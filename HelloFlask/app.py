1c57bff09151cc1fcbe4ef19ad3ea4b9778c92b8
TheClearwind <492071523@qq.com>
2019-04-01 10:44:50 +0800

    学习Flask

--- HelloFlask/app.py
+++ HelloFlask/app.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def hello():
     return "Wellcome to  Flask"
 
+@app.route('/home')
+def hello2():
+    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+# 制定url规则
+
+@app.route('/index/<name>') #对所有的index/XX的路径做出响应
+def hello3(name):
+    return name
 if __name__ == '__main__':
     app.run()
 
